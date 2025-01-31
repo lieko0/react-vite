f09dfcf5c957e1a3e2eda9d86cb59f5f52011fe3
Marco A M <marcohifen@gmail.com>
2025-01-31 17:51:44 -0300

    Initialize base03 with React and Vite setup, including essential components and configuration files

---
+++ base03/src/App.jsx
@@ -0,0 +1,9 @@
+import Header from "./componensts/Header"
+
+export default function App() {
+    return (
+        <div>
+            <Header />
+        </div>
+    )
+}
---
+++ base03/src/componensts/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <header className="main-header">
+            <img className="header-img" src="globe.png" alt="Ícone de Globo" />
+            <span>my travel journal.</span>
+        </header>
+    )
+}
