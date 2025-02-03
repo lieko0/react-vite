0482742302bdad4d9a7c544c78422fde7561459f
Marco A M <marcohifen@gmail.com>
2025-02-03 12:22:47 -0300

    Add Entry component and update App layout with multiple entries

--- base03/src/App.jsx
+++ base03/src/App.jsx
@@ -1,9 +1,15 @@
 import Header from "./componensts/Header"
+import Entry from "./componensts/Entry"
 
 export default function App() {
     return (
         <div>
             <Header />
+            <Entry />
+            <Entry />
+            <Entry />
+            <Entry />
+            <Entry />
         </div>
     )
 }
