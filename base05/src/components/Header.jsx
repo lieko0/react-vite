a1aafabdca42b114a4f19fbac972255b8ab4e019
Marco A M <marcohifen@gmail.com>
2025-02-04 15:04:56 -0300

    Add ingredient management to Main component and welcome alert in Header

--- base05/src/components/Header.jsx
+++ base05/src/components/Header.jsx
@@ -1,8 +1,18 @@
 import chefClaudeIcon from '/chef-claude-icon.png';
 
 export default function Header() {
+
+    function handleMouseClick() {
+        alert("Welcome to Chef Claude's Recipe App!")
+    }
+
+
+
     return (
-        <header className="header-main">
+        <header 
+            className="header-main"
+            onMouseDown={handleMouseClick}
+        >
             <div className="header-main-container">
                 <img className="header-img" src={chefClaudeIcon} alt="Chef Claude Icon" />
                 <span className="header-title">Chef Claude</span>
--- base05/src/components/Main.jsx
+++ base05/src/components/Main.jsx
@@ -1,14 +1,42 @@
+import { useState } from "react"
+
 export default function Main() {
+    const [ingredients, setIngredients] = useState([
+        "Chicken",
+        "Oregano",
+        "Tomatoes",
+        "Garlic",
+        "Onions",
+    ])
+    
+    const ingredientsList = ingredients.map((ingredient, index) => { 
+        return <li key={index}>{ingredient}</li>
+    })
+
+    function handleSubmit(event) {
+        event.preventDefault()
+        const newIngredient = new FormData(event.target).get("add-ingredient")
+        
+        if (!newIngredient || ingredients.includes(newIngredient)) return
+        
+        setIngredients((prevIngredients) => [...prevIngredients, newIngredient])
+        event.target.reset()
+    }
+
     return (
         <main className="main-div">
-            <form className="main-form-add-ingredient">
+            <form onSubmit={handleSubmit} className="main-form-add-ingredient">
                 <input 
                     type="text"
                     placeholder="e.g. oregano"
-                    aria-label="Add ingredient" 
+                    aria-label="Add ingredient"
+                    name="add-ingredient"
                 />
                 <button>Add ingredient</button>
             </form>
+            <ul>
+                {ingredientsList}
+            </ul>
         </main>
     )
 }
