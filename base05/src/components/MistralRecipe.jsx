70f1dafbe4b7a2ab2b20ce16236765bcbec39458
Marco A M <marcohifen@gmail.com>
2025-02-07 14:17:32 -0300

    Add react-markdown dependency and refactor MistralRecipe component to use ReactMarkdown for rendering recipes

--- base05/src/components/MistralRecipe.jsx
+++ base05/src/components/MistralRecipe.jsx
@@ -1,30 +1,14 @@
-import { getRecipeFromMistral } from "../ai"
+import ReactMarkdown from "react-markdown"
 
 export default function MistralRecipe(props) {
-    
-    const formattedRecipe = props.recipe.split("\n").map((line, index) => {
-        if (line.startsWith("### ")) {
-            return <h3 key={index}>{line.replace("### ", "")}</h3>
-        }
-        if (line.startsWith("#### ")) {
-            return <h4 key={index}>{line.replace("#### ", "")}</h4>
-        }
-        if (line.startsWith("**")) {
-            return <h3 key={index}>{line.replaceAll("**", "")}</h3>
-        }
-        if (line.endsWith(":")) {
-            return <h4 key={index}>{line}</h4>
-        }
-        return <p key={index}>{line}</p>
-    })
     
     return (
         <section>
             <h2>Chef Mistral Recommends:</h2>
             <article className="suggested-recipe-container" aria-live="polite">
-                <div>
-                    {formattedRecipe}
-                </div>
+                <ReactMarkdown>
+                    {props.recipe}
+                </ReactMarkdown>
             </article>
         </section>
     )
